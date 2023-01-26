f7aedeaf45a74aaa0cea8a4a9f121f07662886d6
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-26 20:41:12 +0000

    Removed proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/v1/transactions": "http://localhost:3000",
-    },
-  },
   plugins: [react()],
 });
